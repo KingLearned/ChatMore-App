1c1bee4e634c13513de71e59589fb3aac619aee6
kinglearned <kinglearned@gmail.com>
2023-05-02 22:47:06 +0100

    Modifying the content

--- AppModules/signUpModule.js
+++ AppModules/signUpModule.js
@@ -1,5 +1,34 @@
 const MYSQL = require("../MODULES/Conn");
 
+const signUpAPI = (Sig_Name, Sig_Tele, Sig_Pwd, Sig_CPwd) => {
+    const valid = Sig_Name.replace(/[^a-zA-Z^0-9]/g, "")
+      if(Sig_Name == valid){
 
+        if(Sig_Pwd !== Sig_CPwd){
+          res.json({ErrMsg: 'Password Mismatched!'})
+        }else if(Sig_Tele.length < 11 || Sig_Tele.length > 11){
+          res.json({ErrMsg: 'Invalid Phone Number!'})
+        }else if(Sig_Pwd == Sig_CPwd){
+          
+          const About = `Hello, I'm using ChatMore App`
+          const query = 'INSERT INTO `users` (`username`, `telephone`, `pwd`,`about`, `user_img`, `friends`, `chats`) VALUES(?,?,?,?,?,?,?)'
+          MYSQL.query(query, [Sig_Name,Sig_Tele,Sig_Pwd,About,'','',''], (err, result) => {
+            if(err){
+              
+              const ErrName = err.sqlMessage == `Duplicate entry '${Sig_Name}' for key 'users.PRIMARY'` ? `Duplicate entry '${Sig_Name}' for key 'users.PRIMARY'` : `Duplicate entry '${Sig_Name}' for key 'PRIMARY'`
+              const ErrTele = err.sqlMessage == `Duplicate entry '${Sig_Tele}' for key 'users.telephone'` ? `Duplicate entry '${Sig_Tele}' for key 'users.telephone'` : `Duplicate entry '${Sig_Tele}' for key 'telephone'`
+
+              const Error = err.sqlMessage == ErrName ? res.json({ErrMsg: 'Username Already Exist!'}) :
+              err.sqlMessage == ErrTele ? res.json({ErrMsg: 'Number Already Exist!'}) : ''
+              
+            }else{
+              res.json({Successful: 'Registered Succefully!'})
+            }
+          })
+        }
+      }else{
+        res.json({ErrMsg: 'use characters [Aa-Zz & 0-9] only!'})
+      }
+}
 
 // module.exports = { SendMsgAPI }
