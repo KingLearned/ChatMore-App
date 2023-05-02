2bffe4c3ae8a76b3702a42df87e834548cfea6b2
kinglearned <kinglearned@gmail.com>
2023-05-03 00:04:07 +0100

    Modifying the content

--- AppModules/signUpModule.js
+++ AppModules/signUpModule.js
@@ -1,42 +1,40 @@
 const MYSQL = require("../MODULES/Conn");
 
-const signUpAPI = (Sig_Name, Sig_Tele, Sig_Pwd, Sig_CPwd) => {
-    const valid = Sig_Name.replace(/[^a-zA-Z^0-9]/g, "")
-    if(Sig_Name == valid){
+class signUpAPI {
+    constructor(Sig_Name, Sig_Tele, Sig_Pwd, Sig_CPwd){ this.Sig_Name = Sig_Name, this.Sig_Tele = Sig_Tele, this.Sig_Pwd = Sig_Pwd, this.Sig_CPwd = Sig_CPwd }
 
-      if(Sig_Pwd !== Sig_CPwd){
-        return({ErrMsg: 'Password Mismatched!'})
-        // res.json({ErrMsg: 'Password Mismatched!'})
-      }else if(Sig_Tele.length < 11 || Sig_Tele.length > 11){
-        return({ErrMsg: 'Invalid Phone Number!'})
-        // res.json({ErrMsg: 'Invalid Phone Number!'})
-      }else if(Sig_Pwd == Sig_CPwd){
-        
-        const About = `Hello, I'm using ChatMore App`
-        const query = 'INSERT INTO `users` (`username`, `telephone`, `pwd`,`about`, `user_img`, `friends`, `chats`) VALUES(?,?,?,?,?,?,?)'
-        MYSQL.query(query, [Sig_Name,Sig_Tele,Sig_Pwd,About,'','',''], (err, result) => {
-          if(err){
-            
-            const ErrName = err.sqlMessage == `Duplicate entry '${Sig_Name}' for key 'users.PRIMARY'` ? `Duplicate entry '${Sig_Name}' for key 'users.PRIMARY'` : `Duplicate entry '${Sig_Name}' for key 'PRIMARY'`
-            const ErrTele = err.sqlMessage == `Duplicate entry '${Sig_Tele}' for key 'users.telephone'` ? `Duplicate entry '${Sig_Tele}' for key 'users.telephone'` : `Duplicate entry '${Sig_Tele}' for key 'telephone'`
+    Feedback(){
+        const valid = this.Sig_Name.replace(/[^a-zA-Z^0-9]/g, "")
+      if(this.Sig_Name == valid){
 
-            const ErrorMsg = err.sqlMessage == ErrName ? ({ErrMsg: 'Username Already Exist!'}) :
-            err.sqlMessage == ErrTele ? ({ErrMsg: 'Number Already Exist!'}) : ''
+        if(this.Sig_Pwd !== this.Sig_CPwd){
+          return({ErrMsg: 'Password Mismatched!'})
+        //   res.json({ErrMsg: 'Password Mismatched!'})
+        }else if(this.Sig_Tele.length < 11 || this.Sig_Tele.length > 11){
+          return({ErrMsg: 'Invalid Phone Number!'})
+        //   res.json({ErrMsg: 'Invalid Phone Number!'})
+        }else if(this.Sig_Pwd == this.Sig_CPwd){
+          
+          const About = `Hello, I'm using ChatMore App`
+          const query = 'INSERT INTO `users` (`username`, `telephone`, `pwd`,`about`, `user_img`, `friends`, `chats`) VALUES(?,?,?,?,?,?,?)'
+          MYSQL.query(query, [this.Sig_Name,this.Sig_Tele,this.Sig_Pwd,About,'','',''], (err, result) => {
+            if(err){
+              
+              const ErrName = err.sqlMessage == `Duplicate entry '${this.Sig_Name}' for key 'users.PRIMARY'` ? `Duplicate entry '${this.Sig_Name}' for key 'users.PRIMARY'` : `Duplicate entry '${this.Sig_Name}' for key 'PRIMARY'`
+              const ErrTele = err.sqlMessage == `Duplicate entry '${this.Sig_Tele}' for key 'users.telephone'` ? `Duplicate entry '${this.Sig_Tele}' for key 'users.telephone'` : `Duplicate entry '${this.Sig_Tele}' for key 'telephone'`
 
-            // const ErrorMsg = err.sqlMessage == ErrName ? ({ErrMsg: 'Username Already Exist!'}) :
-            // err.sqlMessage == ErrTele ? ({ErrMsg: 'Number Already Exist!'}) : ''
-
-            // return (ErrorMsg)
-            
-          }else{
-            return({Successful: 'Registered Succefully!'})
-            // res.json({Successful: 'Registered Succefully!'})
-          }
-        })
+              const Error = err.sqlMessage == ErrName ? res.json({ErrMsg: 'Username Already Exist!'}) :
+              err.sqlMessage == ErrTele ? res.json({ErrMsg: 'Number Already Exist!'}) : ''
+              
+            }else{
+              return({Successful: 'Registered Succefully!'})
+            //   res.json({Successful: 'Registered Succefully!'})
+            }
+          })
+        }
+      }else{
+        res.json({ErrMsg: 'use characters [Aa-Zz & 0-9] only!'})
       }
-    }else{
-        return({ErrMsg: 'use characters [Aa-Zz & 0-9] only!'})
-    //   res.json({ErrMsg: 'use characters [Aa-Zz & 0-9] only!'})
     }
 }
 
