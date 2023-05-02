d73e440c101b83dc85f8334315fc1161ba9aab79
kinglearned <kinglearned@gmail.com>
2023-05-02 23:02:53 +0100

    Modifying the content

--- AppModules/signUpModule.js
+++ AppModules/signUpModule.js
@@ -6,8 +6,9 @@ const signUpAPI = (Sig_Name, Sig_Tele, Sig_Pwd, Sig_CPwd) => {
 
         if(Sig_Pwd !== Sig_CPwd){
 
-          res.json({ErrMsg: 'Password Mismatched!'})
-         return console.log('Password Mismatched!')
+        //   res.json({ErrMsg: 'Password Mismatched!'})
+            console.log('Password Mismatched!')
+            return {ErrMsg: 'Password Mismatched!'}
 
         }else if(Sig_Tele.length < 11 || Sig_Tele.length > 11){
         //   res.json({ErrMsg: 'Invalid Phone Number!'})
