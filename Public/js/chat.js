a86755e4ae20909390f9b96f0247fa83af249666
kinglearned <kinglearned@gmail.com>
2023-08-15 11:17:48 +0100

    Modified the UI and Modulize some components

--- Public/js/chat.js
+++ Public/js/chat.js
@@ -1,4 +1,4 @@
-import showFriends from "./chatsModule/showFriends"
+import showFriends from "./chatsModule/showFriends.js"
 
 const showImg = (imgName) => {
     return `https://cloud.appwrite.io/v1/storage/buckets/Chatmoreupload/files/${imgName}/view?project=64c7e9ee17c84cabe3cd&mode=admin`
