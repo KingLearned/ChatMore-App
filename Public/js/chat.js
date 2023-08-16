2eca5881da17f50689fcff94b78824050d63831d
kinglearned <kinglearned@gmail.com>
2023-08-16 05:11:41 +0100

    Modifying the chats.js

--- Public/js/chat.js
+++ Public/js/chat.js
@@ -61,9 +61,9 @@ $.ajax({
                 for (let p = 0; p < data.SORT.length; p++) {
                     // Change FriendImg 
                     data.FRD[i] == data.SORT[p].username && data.SORT[p].user_img !== '' ? FriendImg =  `${showImg(data.SORT[p].user_img)}` : ''
-                    if(data.FRD[i] == data.SORT[p].username){
-                        $('about').html(data.SORT[p].about) //For about the user friend
-                    }
+
+                    //Display Friends About
+                    data.FRD[i] == data.SORT[p].username && $('about').html(data.SORT[p].about)
                 }
 
                 const setStatus = localStorage.getItem(data.FRD[i])
