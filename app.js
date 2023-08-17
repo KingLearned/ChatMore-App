435e3bb644ab1843fc298b577da879b9cedae826
kinglearned <kinglearned@gmail.com>
2023-08-17 13:56:48 +0100

    Added the vercel module for hosting

--- app.js
+++ app.js
@@ -308,34 +308,23 @@ app.post('/', upload.single('User_Img'), (req, res) => {
       })
 
     }else if(Sig_Name, Sig_Tele, Sig_Pwd, Sig_CPwd) {/********************************* SIGN UP HANDLER  ********************************/
-      const valid = Sig_Name.replace(/[^a-zA-Z^0-9]/g, "")
-      if(Sig_Name == valid){
-
-        if(Sig_Pwd !== Sig_CPwd){
-          res.json({ErrMsg: 'Password Mismatched!'})
-        }else if(Sig_Pwd == Sig_CPwd){
+      
+      res.json({ErrMsg: 'Registered Succefully!'})
+      // const About = `Hello, I'm using ChatMore App`
+      // const query = 'INSERT INTO `users` (`username`, `telephone`, `pwd`,`about`, `user_img`, `friends`, `chats`) VALUES(?,?,?,?,?,?,?)'
+      // MYSQL.query(query, [Sig_Name,Sig_Tele,Sig_Pwd,About,'','',''], (err, result) => {
+      //   if(err){
           
-          res.json({ErrMsg: 'Registered Succefully!'})
-          // const About = `Hello, I'm using ChatMore App`
-          // const query = 'INSERT INTO `users` (`username`, `telephone`, `pwd`,`about`, `user_img`, `friends`, `chats`) VALUES(?,?,?,?,?,?,?)'
-          // MYSQL.query(query, [Sig_Name,Sig_Tele,Sig_Pwd,About,'','',''], (err, result) => {
-          //   if(err){
-              
-          //     const ErrName = err.sqlMessage == `Duplicate entry '${Sig_Name}' for key 'users.PRIMARY'` ? `Duplicate entry '${Sig_Name}' for key 'users.PRIMARY'` : `Duplicate entry '${Sig_Name}' for key 'PRIMARY'`
-          //     const ErrTele = err.sqlMessage == `Duplicate entry '${Sig_Tele}' for key 'users.telephone'` ? `Duplicate entry '${Sig_Tele}' for key 'users.telephone'` : `Duplicate entry '${Sig_Tele}' for key 'telephone'`
+      //     const ErrName = err.sqlMessage == `Duplicate entry '${Sig_Name}' for key 'users.PRIMARY'` ? `Duplicate entry '${Sig_Name}' for key 'users.PRIMARY'` : `Duplicate entry '${Sig_Name}' for key 'PRIMARY'`
+      //     const ErrTele = err.sqlMessage == `Duplicate entry '${Sig_Tele}' for key 'users.telephone'` ? `Duplicate entry '${Sig_Tele}' for key 'users.telephone'` : `Duplicate entry '${Sig_Tele}' for key 'telephone'`
 
-          //     const Error = err.sqlMessage == ErrName ? res.json({ErrMsg: 'Username Already Exist!'}) :
-          //     err.sqlMessage == ErrTele ? res.json({ErrMsg: 'Number Already Exist!'}) : ''
-              
-          //   }else{
-          //     res.json({Successful: 'Registered Succefully!'})
-          //   }
-          // })
-        }
-      }else{
-        res.json({ErrMsg: 'use characters [Aa-Zz & 0-9] only!'})
-      }
-
+      //     const Error = err.sqlMessage == ErrName ? res.json({ErrMsg: 'Username Already Exist!'}) :
+      //     err.sqlMessage == ErrTele ? res.json({ErrMsg: 'Number Already Exist!'}) : ''
+          
+      //   }else{
+      //     res.json({Successful: 'Registered Succefully!'})
+      //   }
+      // })
     }
 
   } 
