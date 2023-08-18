60fcd5f849b019d0ddae34a01a917015eafebde2
kinglearned <kinglearned@gmail.com>
2023-08-18 17:26:16 +0100

    Added Cors

--- AppModules/sendmsg.js
+++ AppModules/sendmsg.js
@@ -1,9 +1,6 @@
 const MYSQL = require("../MODULES/Conn");
 const { Emoji, EmojiId } = require("./Emojis");
 
-
-
-
 class SendMsgAPI {
     constructor(LOGIN, ChatMsg, MsgTo, ElementTag){ this.LOGIN = LOGIN, this.ChatMsg = ChatMsg, this.MsgTo = MsgTo, this.ElementTag = ElementTag }
 
