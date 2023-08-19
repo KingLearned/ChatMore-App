0873197fb5021600b7b0f73128c5f665062a16ea
kinglearned <kinglearned@gmail.com>
2023-08-19 15:46:38 +0100

    Added the direct path socket.io

--- index.js
+++ index.js
@@ -344,6 +344,4 @@ io.on('connection', (socket) => {
 })
 
 
-server.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
-})
+server.listen(PORT, () => {  console.log(`listening on ${PORT}`) })
