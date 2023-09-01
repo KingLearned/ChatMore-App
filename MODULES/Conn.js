2724cc9ad0c40a1f2126c283430a3bb569496b06
kinglearned <kinglearned@gmail.com>
2023-09-01 20:11:27 +0100

    Handle the state management function in login, register and write pages

--- MODULES/Conn.js
+++ MODULES/Conn.js
@@ -18,7 +18,7 @@ MYSQL.connect((err, result) => {
     if(err, result){
         console.log('Data Base Initiated!')
     }else{
-        console.log('Data Base Not Found!\nSTART Apache & MySQL from Xampp API')
+        console.log('Data Base Not Connected')
     }
 })
 
