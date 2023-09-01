06f6b5e98572156b71562b01dba2ac58edfb60c8
kinglearned <kinglearned@gmail.com>
2023-09-01 21:22:40 +0100

    Making some few changes

--- index.js
+++ index.js
@@ -334,7 +334,6 @@ app.post('/', upload.single('User_Img'), (req, res) => {
 })
 
 io.on('connection', (socket) => {
-
   socket.on('chat message', (Msg,Exp) => {
     io.emit('chat message', Msg,Exp)
   })
