3c42a5d11ea9e641a64ed550ac79417c33ece905
kinglearned <kinglearned@gmail.com>
2023-09-09 12:39:35 +0100

    Making some few changes

--- MODULES/Conn.js
+++ MODULES/Conn.js
@@ -1,18 +1,18 @@
 const SQL = require('mysql')
 
-const MYSQL = SQL.createConnection({
-    host: "Localhost",
-    user: "root",
-    password: 'Learned 1945',
-    database: 'chatmoreapp'
-})
-
 // const MYSQL = SQL.createConnection({
-//     host: "db4free.net",
-//     user: "learnedsconcept",
-//     password: "m94jC6bS3Xp!2LR",
-//     database: "lxpurchase"
+//     host: "Localhost",
+//     user: "root",
+//     password: 'Learned 1945',
+//     database: 'chatmoreapp'
 // })
+
+const MYSQL = SQL.createConnection({
+    host: "db4free.net",
+    user: "learnedsconcept",
+    password: "m94jC6bS3Xp!2LR",
+    database: "lxpurchase"
+})
 
 MYSQL.connect((err, result) => {
     if(err, result){
